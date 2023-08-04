318d80d6d69e70a439725325a9e588e1d302dcf8
adit0909090909090909 <madityailham35@gmail.com>
2023-08-04 14:20:39 +0700

    mario game

--- js/script.js
+++ js/script.js
@@ -1,41 +1,26 @@
-const mario = document.querySelector('.mario');
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
 
+const jump =() =>{
+    mario.classList.add('jump')
 
-const jump = () => {
-   mario.classList.add('jump');
-
-
-   setTimeout(() => {
-       mario.classList.remove('jump');
-   }, 500);
+    setTimeout(() => {
+        mario.classList.remove('jump')
+    }, 500)
 }
 
-
-document.addEventListener('keydown', jump);
-
-
-
-const pipe = document.querySelector('.pipe');
 const loop = setInterval(() => {
-   const pipePosition = pipe.offsetLeft;
-
-
-   if (pipePosition <= 120) {
-       pipe.style.animation = "none";
-       pipe.style.left = `${pipePosition}px`;
-   }
-}, 10)
-
-
-const loop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = window.getComputedStyle(mario).bottom.replace('px','');
- 
- 
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','')
+    
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-        pipe.style.animation = "none";
-        pipe.style.left = `${pipePosition}px`;
+        pipe.style.animation = "none"
+        pipe.style.left = `${pipePosition}px`
+        mario.style.animation ="none"
+        mario.style.bottom = `${marioPosition}px`
+        mario.src = './images/game-over.png'
+        mario.style.width = "75px"
+        mario.style.marginLeft = "50px"
     }
- }, 10)
- 
- 
+}, 10);
+document.addEventListener('keydown', jump)
